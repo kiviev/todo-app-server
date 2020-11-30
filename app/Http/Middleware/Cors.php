822087abfd70eb369d7409cff4898a57b1569e24
Paco Alba <kiviev@gmail.com>
2020-11-30 15:05:04 +0100

    Cors resolved

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,11 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://ofertaka.com')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            //Url a la que se le dará acceso en las peticiones
+            ->header("Access-Control-Allow-Origin", env('VUE_APP_SERVER', 'http://localhost:8080'))
+            //Métodos que a los que se da acceso
+            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+            //Headers de la petición
+            ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
     }
 }
